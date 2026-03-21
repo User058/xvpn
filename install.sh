790095111d931d1e2494766e05f253bcbb0bc7a2
User058 <110759720+User058@users.noreply.github.com>
2026-03-21 13:54:15 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -279,7 +279,7 @@ apply_firewall_rules() {
     if grep -q "--dport 22" /etc/iptables/rules.v4; then
       sed -i "/--dport 22 -j ACCEPT/a \\n-A INPUT -p tcp -m state --state NEW -m tcp --dport 80 -j ACCEPT\n-A INPUT -p tcp -m state --state NEW -m tcp --dport 443 -j ACCEPT\n-A INPUT -p tcp -m state --state NEW -m tcp --dport 8080 -j ACCEPT" /etc/iptables/rules.v4
     else
-      echo "-A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT" >> /etc/iptables/rules.v4
+      #echo "-A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT" >> /etc/iptables/rules.v4
       echo "-A INPUT -p tcp -m state --state NEW -m tcp --dport 80 -j ACCEPT" >> /etc/iptables/rules.v4
       echo "-A INPUT -p tcp -m state --state NEW -m tcp --dport 443 -j ACCEPT" >> /etc/iptables/rules.v4
       echo "-A INPUT -p tcp -m state --state NEW -m tcp --dport 8080 -j ACCEPT" >> /etc/iptables/rules.v4
@@ -335,7 +335,23 @@ final_cleanup() {
     
     log_success "Final cleanup done."
 }
-
+# configure profile
+configure_profile() {
+	log_info "Configuring Profile..."
+cat> /root/.profile << END
+# ~/.profile: executed by Bourne-compatible login shells.
+if [ "$BASH" ]; then
+  if [ -f ~/.bashrc ]; then
+    . ~/.bashrc
+  fi
+fi
+mesg n || true
+clear
+menu
+END
+chmod 644 /root/.profile
+    log_success "profile updated."
+}
 # Main function that orchestrates the installation
 main() {
     # Initial setup and validation
